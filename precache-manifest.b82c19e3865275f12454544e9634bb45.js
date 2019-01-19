fbe8d1f18d823ce5872c4a05887f0caeddaca4c2
George Andersen <george.andersen@obextech.com>
2019-01-19 17:38:21 +0000

    Updates

---
+++ precache-manifest.b82c19e3865275f12454544e9634bb45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "9ac66f6f5101410e07b7",
+    "url": "/myapp/static/js/main.9ac66f6f.chunk.js"
+  },
+  {
+    "revision": "7644177116356cf21c79",
+    "url": "/myapp/static/js/1.76441771.chunk.js"
+  },
+  {
+    "revision": "9ac66f6f5101410e07b7",
+    "url": "/myapp/static/css/main.972b3ed5.chunk.css"
+  },
+  {
+    "revision": "44eced193b8721ab01c6fe52b7be2c4d",
+    "url": "/myapp/index.html"
+  }
+];
